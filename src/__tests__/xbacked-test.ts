371fff9e86f9de51a6152b93144a390bd0ee91f7
Akingbesote Jesulonimi <anjola4jeez@gmail.com>
2021-11-17 22:07:12 +0100

    Add: dependencies (#1)
    * Add: dependenciesp
    
    * Update: change to yarn
    
    * Add: github actions
    
    * Add: github actions
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Add: yarnrc file
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Remove: yarnrc.yml
    
    * Update test.yml
    
    * Add: lint  to action
    
    * Add: lin to action
    
    * Update: github actions test file
    
    * Update: github actions test file
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml

---
+++ src/__tests__/xbacked-test.ts
@@ -0,0 +1,5 @@
+import { xbacked } from '../index';
+
+test('xbacked', () => {
+  expect(xbacked('x')).toBe('x');
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const xbacked = (client: string) => {
+  return client;
+};
