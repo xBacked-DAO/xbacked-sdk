c25ac1c17e26108f8408db5262bbb3deff5b45ed
jesulonimi21 <anjola4jeez@gmail.com>
2021-12-09 16:35:28 +0100

    Remove: xbacked-da0/contracts

--- examples/connectFeeCollectorToVault.js
+++ examples/connectFeeCollectorToVault.js
@@ -4,6 +4,6 @@ import pkg from 'xbacked-sdk';
   const feeCollector = new FeeCollector();
   const acc = new Account({signer: 'MyAlgoConnect',
     network: 'TestNet', interact: feeCollector});
-  await acc.connectToVault({vault: new Vault({id: 186})});
+  await acc.connectToVault({vault: new Vault({id: VAULT_ID})});
   console.log('opt in successful');
 })();
--- examples/connectReserveToVault.js
+++ examples/connectReserveToVault.js
@@ -5,7 +5,7 @@ const {Account, Reserve, Vault} = pkg;
   const acc = new Account({signer: 'MyAlgoConnect',
     network: 'TestNet', interact: reserve});
   acc.addListener('getCollateralPrice', async () => 2);
-  await acc.connectToVault({vault: new Vault({id: 186})});
+  await acc.connectToVault({vault: new Vault({id: VAULT_ID})});
   console.log('connected successful');
 })();
 
