98496da06ca1d0868a1579a0a36e97f2fb00e645
Austin Wilshire <awoldes@gmail.com>
2022-01-26 15:16:14 +1000

    FIX: missing modules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 /* tslint:disable no-var-requires */
 const Account = require('./Account').default;
-const Minter = require('./interacts/Minter').default;
-const Reserve = require('./interacts/Reserve').default;
 const Vault = require('./Vault').default;
-const FeeCollector = require('./interacts/FeeCollector').default;
 const xbacked = (client: string) => {
   return client;
 };
