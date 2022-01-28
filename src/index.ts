01381f020f22a9817a9532be00ed62100910f4bc
Juan C. Castrejon <castrejon@ciencias.unam.mx>
2022-01-28 14:59:59 -0600

    UPDATE: add EventFetcher to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 /* tslint:disable no-var-requires */
 const Account = require('./Account').default;
 const Vault = require('./Vault').default;
+const EventFetcher = require('./EventFetcher').default;
 const xbacked = (client: string) => {
   return client;
 };
 
-module.exports = { Account, Vault };
+module.exports = { Account, Vault, EventFetcher };
