692d59a7fcd6014e61c635497f00da993de1147f
Juan C. Castrejon <castrejon@ciencias.unam.mx>
2022-01-31 10:23:28 -0600

    Add comment to formula

--- src/utils.ts
+++ src/utils.ts
@@ -18,6 +18,9 @@ export const convertFromMicroUnits = (val: number): number => {
 
 // Calculates the max amount of debt you can pay to drive the CR
 // back to 120, considering collateral goes down on each liquidation.
+// The formula solves for the max debt payout using the formula for calculating collateralRatio in the contract
+// given that we know it needs to be 120 minimum. Where:
+// m = masDebtPayout, c = collateral, p = collateralPrice, d = vaultDebt, r = MINIMUM_COLLATERAL_RATIO = 120, 0.035 = DISCOUNT_RATE
 // https://www.symbolab.com/solver/equation-calculator/solve%20for%20m%2C%20r%20%3D%20%5Cfrac%7B%5Cleft(%5Cleft(c%20-%5Cleft(%5Cfrac%7Bm%7D%7B%5Cleft(p%20-%20%5Cleft(p%5Ccdot0.035%5Cright)%5Cright)%7D%20%5Cright)%5Cright)%5Ccdot%20p%5Ccdot100%5Cright)%7D%7Bd%20-%20m%7D?or=input
 export const calcMaxDebtPayout = (collateral: number, collateralPrice: number, vaultDebt: number): number => {
   const discountRateInv = 1 - DISCOUNT_RATE;
