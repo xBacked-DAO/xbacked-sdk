f5a4374755e7eef4eb4fe7e540cab732588916e4
Juan C. Castrejón <castrejon@ciencias.unam.mx>
2022-02-21 12:02:05 -0600

    FIX: exports for ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* tslint:disable no-var-requires */
-const { Account } = require('./Account');
-const { Vault } = require('./Vault').default;
+import { Account } from './Account'
+import { Vault } from './Vault'
 import * as VaultDiscovery from './vaultDiscovery';
 import * as Utils from './utils';
 
