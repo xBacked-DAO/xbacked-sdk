8b4d49dc26a84c2bd31e4e7c80de72c7d06a04ba
mrcordova <46950394+mrcordova@users.noreply.github.com>
2022-03-25 15:29:46 -0700

    Update redemptions.js
    changed function name from "proposeVault" to "proposeVaultForRedemption".

--- examples/redemptions.js
+++ examples/redemptions.js
@@ -8,7 +8,7 @@ const { Account, Vault } = require('..');
   const vault = new Vault({ id: 123 });
 
   // receive 0.75% of the collateral of the vault, protocol takes 0.25%
-  await account.proposeVault({ vault, address: 'wallet address of user vault' });
+  await account.proposeVaultForRedemption({ vault, address: 'wallet address of user vault' });
 
   // redeem 100 xUSD tokens & receive $100 worth of collateral
   await account.redeemVault({ vault, address: 'wallet address of user vault', amountToRedeem: 100 });
