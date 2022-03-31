a7c6f8c18b6abbd588df92073ca78b0c896ff4e1
jesulonimi21 <anjola4jeez@gmail.com>
2022-03-31 12:47:36 +0100

    Update: getState.js

--- examples/getState.js
+++ examples/getState.js
@@ -1,11 +1,31 @@
 /* eslint-disable no-unused-vars */
+const {ask} = require('@reach-sh/stdlib');
 const {Account, Vault} = require('..');
+const dotenv = require('dotenv');
+dotenv.config();
 (async () => {
-  const acc = new Account({
-    mnemonic: 'lens sell....',
-    network: 'LocalHost',
+  const mnemonic = process.env.MNEMONIC;
+  const VAULT_ID = process.env.VAULT_ID;
+  const account = new Account({
+    mnemonic,
+    network: 'TestNet',
   });
+  const vault = new Vault({id: VAULT_ID});
   // eslint-disable-next-line max-len
-  const userInfo = await acc.getUserInfo({vault: new Vault({id: 146}), address: ''});
-  const vaultState = await acc.getVaultState({vault: new Vault({id: 146})});
+  while (true) {
+    const action = await ask.ask(`
+  Do you want to:
+  1. get user info
+  2. get vault state
+  `, parseInt);
+    switch (action) {
+      case 1: const userInfo = await account.getUserInfo({vault,
+        address: await account.getAddress()});
+        console.log(userInfo);
+        break;
+      case 2: const vaultState = await account.getVaultState({vault});
+        console.log(vaultState);
+        break;
+    }
+  }
 })();
