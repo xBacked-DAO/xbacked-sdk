3a940f5c2b8d466893d21d24014e62d6e4ddaa89
Juan C. Castrejón <castrejon@ciencias.unam.mx>
2022-04-18 13:23:47 -0600

    UPDATE: add info  to example

--- examples/liquidatorEvents.js
+++ examples/liquidatorEvents.js
@@ -1,14 +1,22 @@
 
+/*
+  This example script yields all the liquidation events that occured in the
+  current vault version.
+
+  Usage example:
+  $ VAULT_ID=79758986 node examples/liquidatorEvents.js | tee log.txt
+*/
+
 const { masterVault } = require('@xbacked-dao/xbacked-contracts');
 const axios = require('axios');
 const {
   Account,
   Vault,
   getTransactions
-} = require('..'); // Use require('@xbacked-dao/xbacked-sdk'); instead
+} = require('..'); // Use require('@xbacked-dao/xbacked-sdk'); instead if using sdk as module
 
 // Current Testnet ID's
-const VAULT_ID = 70423635;
+const VAULT_ID = process.env.VAULT_ID;
 const TOKEN_ID = 62281549;
 
 const NETWORK = 'TestNet';
@@ -21,43 +29,48 @@ const startRound = 0;
 const timeout = 5 * 60 * 1000;
 
 (async () => {
-  const account = new Account({
-    mnemonic: 'amateur sponsor crystal rain filter bulk document silver erupt wave science enough half access crack illegal such broom tobacco dress napkin faint way ability result',
-    network: NETWORK
-  });
-  await account.optIntoToken(TOKEN_ID);
+  if (process.env.VAULT_ID) {
+    const account = new Account({
+      mnemonic: 'amateur sponsor crystal rain filter bulk document silver erupt wave science enough half access crack illegal such broom tobacco dress napkin faint way ability result',
+      network: NETWORK
+    });
+    await account.optIntoToken(TOKEN_ID);
 
-  account.fundFromFaucet();
-  const vault = new Vault({id: VAULT_ID});
+    account.fundFromFaucet();
+    const vault = new Vault({id: VAULT_ID});
 
-  console.log(`Getting transactions history starting from round ${startRound}...`);
-  let liquidations = 0, txns = 0;
+    console.log(`Getting transactions history starting from round ${startRound}...`);
+    let liquidations = 0, txns = 0;
 
-  // This function will be executed each time a new transaction is found.
-  const f = async (event) => {
-    const current_liq = liquidations;
-    const current_txns = txns;
-    txns++;
-    if (event.type === 'LIQUIDATE') {
-      liquidations++;
-      // Async GET will mess up transaction order.
-      const res = await axios.get(`https://algoindexer.testnet.algoexplorerapi.io/v2/blocks/${event.round}`)
-      console.log(event);
+    // This function will be executed each time a new transaction is found.
+    const f = async (event) => {
+      const current_liq = liquidations;
+      const current_txns = txns;
+      txns++;
+      if (event.type === 'LIQUIDATE') {
+        liquidations++;
+        // Async GET will mess up transaction order.
+        const res = await axios.get(`https://algoindexer.testnet.algoexplorerapi.io/v2/blocks/${event.round}`)
+        console.log(event);
 
-      const d = new Date(res.data.timestamp * 1000);
-      console.log(`Liquidation time: ${d.toLocaleString()}`);
+        const d = new Date(res.data.timestamp * 1000);
+        console.log(`Liquidation time: ${d.toLocaleString()}`);
 
-      console.log(`Liq. count: ${current_liq + 1}`);
-      console.log(`Txn. count: ${current_txns + 1}\n`);
-    }
-    if(txns % 1000 == 0) {
-      console.log(`Txn. count: ${txns}\n`);
-    }
-  };
+        console.log(`Liq. count: ${current_liq + 1}`);
+        console.log(`Txn. count: ${current_txns + 1}\n`);
+      }
+      if(txns % 1000 == 0) {
+        console.log(`Txn. count: ${txns}\n`);
+      }
+    };
 
-  await getTransactions({vault, account, f, startRound, timeout});
-  console.log(`\nTimed out after ${timeout / 1000}s`);
-  process.exit(0);
+    await getTransactions({vault, account, f, startRound, timeout});
+    console.log(`\nTimed out after ${timeout / 1000}s`);
+    process.exit(0);
+  } else {
+    console.log(`\nVerify VAULT_ID env is set to the lastest TestNet contract version`);
+    process.exit(1);
+  }
 })();
 
 
