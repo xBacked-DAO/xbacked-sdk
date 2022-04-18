7a0feb5cf3e0de7c62aa9d3db3ef652ef06005e5
Austin Wilshire <awoldes@gmail.com>
2022-04-18 10:24:46 +1000

    ADD: relevant changes from 6359d02

--- src/StakingClient.ts
+++ src/StakingClient.ts
@@ -5,10 +5,33 @@ import { liquidationStaking as backend } from '@xbacked-dao/xbacked-contracts';
 import { Account } from './Account';
 import { AbiInterface, AccountInterface } from './interfaces';
 
+export interface StakeGlobalView {
+  stakingAssetID: number;
+  policy: {
+    status: number;
+    rewardMethod: number;
+    rewardRate: number;
+    applicationType: number;
+    time: number;
+  };
+  totalStake: number;
+  totalRewards: number;
+  totalUsers: number;
+}
+
+export interface StakeLocalView {
+  amountStaked: number;
+  rewardsClaimed: number;
+  lastClaim: number;
+  found: boolean;
+}
 export class StakingClient extends Account {
+  /** @property Unique identifier for the contract */
+  readonly id: number | undefined;
  
-  constructor(params: AccountInterface) {
+  constructor(params: AccountInterface, contractId: number) {
     super(params);
+    this.id = contractId;
   }
 
   /**
@@ -31,6 +54,54 @@ export class StakingClient extends Account {
     await this.initialiseReachAccount();
     const ctc = this.reachAccount.contract(backend, params.contractId);
     return await ctc.getABI();
+  }
+  
+  async getState(params: { account: Account }): Promise<StakeGlobalView> {
+    const ctc = params.account.reachAccount.contract(backend, this.id);
+    const get = ctc.v.State;
+    const stateView = await get.read();
+    if (stateView[0] === 'None') {
+      throw new Error('The view is null');
+    }
+    const stakeState = stateView[1];
+    return {
+      stakingAssetID: stakeState.stakingAsset.toNumber(),
+      policy: {
+        applicationType: stakeState.policy.applicationType.toNumber(),
+        status: stakeState.policy.status.toNumber(),
+        time: stakeState.policy.time.toNumber(),
+        rewardMethod: stakeState.policy.rewardMethod.toNumber(),
+        rewardRate: stakeState.policy.rewardRate.toNumber()
+      },
+      totalStake: stakeState.totalStake.toNumber(),
+      totalUsers: stakeState.totalUsers.toNumber(),
+      totalRewards: stakeState.totalRewards.toNumber()
+    };
+  }
+
+  async getUserInfo(params: { account: Account; address: string }): Promise<StakeLocalView> {
+    const ctc = params.account.reachAccount.contract(backend, this.id);
+    const get = ctc.v.State;
+    const stateView = await get.readUserAccount(params.address);
+    if (stateView[1][0] === 'None') {
+      return {
+        amountStaked: stateView.amountStaked.toNumber(),
+        rewardsClaimed: stateView.rewardsClaimed.toNumber(),
+        lastClaim: stateView.lastClaim.toNumber(),
+        found: false
+      };
+    }
+    const stakingState = stateView[1][1];
+    return { ...StakingClient.parseUserInfo(stakingState), found: true };
+  }
+
+  static parseUserInfo(stakingState: any): StakeLocalView {
+    return {
+      amountStaked: stakingState.amountStaked.toNumber(),
+      rewardsClaimed: stakingState.rewardsClaimed.toNumber(),
+      lastClaim: stakingState.lastClaim.toNumber(),
+      found: stakingState.found,
+    };
   }
 
 }
