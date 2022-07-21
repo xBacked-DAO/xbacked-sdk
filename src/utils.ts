19557701e8c33056671f5e7533d51ee3107c59f3
jesulonimi21 <anjola4jeez@gmail.com>
2022-07-21 07:52:46 +0100

    remove: object reference for asa vaults

--- src/utils.ts
+++ src/utils.ts
@@ -15,25 +15,7 @@ export const VAULTS = {
     algo: {
       vaultId: 100233411,
       liquidatorDiscount: 0.045,
-    },
-    gobtc: {
-      vaultId: 88664145,
-      assetId: 67396528,
-      assetDecimals: 8,
-      liquidatorDiscount: 0.065,
-    },
-    goeth: {
-      vaultId: 88664271,
-      assetId: 76598897,
-      assetDecimals: 8,
-      liquidatorDiscount: 0.065,
-    },
-    dAlgo: {
-      vaultId: 96463733,
-      assetId: 96202534,
-      assetDecimals: 6,
-      liquidatorDiscount: 0.045,
-    },
+    }
   },
 };
 
