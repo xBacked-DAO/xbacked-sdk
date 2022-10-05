da4c6f9d6fd3d31c2483a00654b37369a85e6324
Juan C. Castrejón <castrejon@ciencias.unam.mx>
2022-10-04 19:42:28 -0500

    FIX: lint and format

--- src/decryptorUtils.ts
+++ src/decryptorUtils.ts
@@ -6,22 +6,22 @@ import AWS from 'aws-sdk';
  */
 type STSParams = {
   /** @property The user's access key id */
-  accessKeyId: string,
+  accessKeyId: string;
   /** @property The user's access key secret */
-  secretAccessKey: string,
+  secretAccessKey: string;
   /** @property The region to log the user in */
-  region: string
-}
+  region: string;
+};
 
 /**
  * Specifies the Assume Role object used by the getCredentials method
  */
 type AssumeRoleSpec = {
   /** @property The ARN of the role to assume */
-  RoleArn: string,
+  RoleArn: string;
   /** @property The name that the session will use */
-  RoleSessionName: string,
-}
+  RoleSessionName: string;
+};
 
 /**
  * Obtains access credentials assuming an elegible KMS role, given valid AWS
@@ -30,13 +30,16 @@ type AssumeRoleSpec = {
  * @param assumeRoleSpec type of [[AssumeRoleSpec]]
  * @returns Promise type of [[AWS.STS.Credentials]]
  */
-export function getCredentials(params: STSParams, assumeRoleSpec: AssumeRoleSpec): Promise<AWS.STS.Credentials | undefined> {
+export function getCredentials(
+  params: STSParams,
+  assumeRoleSpec: AssumeRoleSpec,
+): Promise<AWS.STS.Credentials | undefined> {
   const sts = new AWS.STS(params);
 
   return new Promise((resolve, reject) => {
-    sts.assumeRole(assumeRoleSpec, function(err, data) {
+    sts.assumeRole(assumeRoleSpec, (err, data) => {
       if (err) {
-        throw(err);
+        throw err;
       } else {
         resolve(data.Credentials);
       }
@@ -50,21 +53,21 @@ export function getCredentials(params: STSParams, assumeRoleSpec: AssumeRoleSpec
  * @param credentials type of [[AWS.STS.Credentials]]
  * @returns Promise type of [[String]]
  */
-export function decrypt(buffer: any, credentials?: AWS.STS.Credentials, ): Promise<String | undefined> {
+export function decrypt(buffer: any, credentials?: AWS.STS.Credentials): Promise<string | undefined> {
   const kms = new AWS.KMS({
     accessKeyId: credentials?.AccessKeyId,
     secretAccessKey: credentials?.SecretAccessKey,
     sessionToken: credentials?.SessionToken,
-    region: 'us-west-2'
+    region: 'us-west-2',
   });
 
   return new Promise((resolve, reject) => {
     const params = {
-      CiphertextBlob: buffer
+      CiphertextBlob: buffer,
     };
     kms.decrypt(params, (err, data) => {
       if (err) {
-        throw(err);
+        throw err;
       } else {
         resolve(data.Plaintext?.toString());
       }
