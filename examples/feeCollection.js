e0df644bde7a9d6698405924f8aec072ce715638
jesulonimi21 <anjola4jeez@gmail.com>
2022-11-18 14:44:21 +0100

    remove: settleInterest from examples

--- examples/feeCollection.js
+++ examples/feeCollection.js
@@ -3,7 +3,6 @@ const {ask} = require('@reach-sh/stdlib');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 (async () => {
   const mnemonic = process.env.MNEMONIC;
   const VAULT_ID = VAULTS.TestNet.algo.vaultId;
@@ -14,27 +13,25 @@ dotenv.config();
   const vault = new Vault({id: VAULT_ID});
 
   while (true) {
-    const action = await ask.ask(`
+    const action = await ask.ask(
+        `
   Do you want to:
   1. collect fees
-  2. settle interest
-  3. drip interest
-  `, parseInt);
+  2. drip interest
+  `,
+        parseInt,
+    );
     try {
       switch (action) {
-      // distribute collateral fees to xUSD stakers and DAO
-      // Receive 0.5% of the accured fees
-        case 1: const isFeeCollected = await account.collectFees({vault});
+        // distribute collateral fees to xUSD stakers and DAO
+        // Receive 0.5% of the accured fees
+        case 1:
+          const isFeeCollected = await account.collectFees({vault});
           console.log(isFeeCollected);
           break;
-          // Settle accrued interest
-          // & distribute to DAO & Governance token stakers
-        case 2: const isInterestSettled = await account.settleInterest({vault});
-          console.log(isInterestSettled);
-          break;
-          // Drip interest for specific vault. This is helpful when liquidating
-        case 3: const isInterestDripped = await account
-            .dripInterest({address: await account.getAddress(), vault});
+        // Drip interest for specific vault. This is helpful when liquidating
+        case 2:
+          const isInterestDripped = await account.dripInterest({address: await account.getAddress(), vault});
           console.log(isInterestDripped);
           break;
       }
