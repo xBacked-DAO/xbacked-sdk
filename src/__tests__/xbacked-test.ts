eccb9cef50d6eb1c2b10270c393b09fcbab147a2
Juan Castrejon <jayz@xbacked.io>
2023-01-16 06:16:00 +0000

    FIX use maxCollateralValue prop

--- src/__tests__/xbacked-test.ts
+++ src/__tests__/xbacked-test.ts
@@ -96,8 +96,7 @@ it('Get vault Info', async () => {
       contractState: 0,
       feeStructure: [1, 5, 50, 50],
       minimumDebtAmount: 10,
-      // TODO: re rc-0.0.25
-      // maximumCollateralValue: 10000,
+      maximumCollateralValue: 10000,
     },
     addresses: {
       govStakersAddress: 'CKFJQPYSGJBRDZ7YKJSOTWJOLUBM7HGIPY6MFLQTBFLHHPIOAX3VEZQP44',
@@ -218,8 +217,7 @@ it('Admin set properties', async function () {
       treasuryAddress: 'CKFJQPYSGJBRDZ7YKJSOTWJOLUBM7HGIPY6MFLQTBFLHHPIOAX3VEZQP44',
       feeStructure: [1, 5, 50, 50],
       minimumDebtAmount: 10,
-      // TODO: re rc-0.0.25
-      // maximumCollateralValue: 10000,
+      maximumCollateralValue: 10000,
     },
     vault: new Vault({ id: VAULT_ID }),
   });
