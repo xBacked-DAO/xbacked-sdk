3ded788b6d96f4b795f1ff23deb9a9334715022d
jesulonimi21 <anjola4jeez@gmail.com>
2024-10-15 08:26:30 +0100

    october 15

--- examples/VaultOwner.js
+++ examples/VaultOwner.js
@@ -11,8 +11,8 @@ dotenv.config();
   const STABLECOIN = process.env.STABLE_COIN;
   const acc = new VaultsClient({mnemonic,
     network: 'TestNet'});
-  console.log(await acc.getAddress())
-  const vaultState = await acc.getVaultState({vault: new Vault({name: "goEth", network: acc.network})});
+  console.log(await acc.getAddress());
+  const vaultState = await acc.getVaultState({vault: new Vault({name: 'lfty0314', network: acc.network})});
   const price = convertFromMicroUnits(vaultState.coldState.collateralPrice);
   while (true) {
     const todo = await ask.ask(`
@@ -25,42 +25,47 @@ dotenv.config();
    6.) to deposit collateral`, parseInt);
 
     try {
-
       switch (todo) {
         // should also opt into stable coin
-        case 1: await acc.optIntoToken(VAULTS.TestNet.goEth.assetId);
+        case 1: await acc.optIntoToken(VAULTS.TestNet.lfty0314.assetId);
           console.log('Opted into token');
           break;
-        case 2: const isVaultCreated = await acc.createVault({collateral: 0.047325,
-          mintAmount: 100,
-          vault: new Vault({name: "goEth", network: acc.network}),
-          minimumPrice: price - price * 0.02,
-          maximumPrice: price + price * 0.02});
+        case 2: const isVaultCreated =
+         await acc.createVault({collateral: 5,
+           mintAmount: 100,
+           vault: new Vault({name: 'lfty0314', network: acc.network}),
+           minimumPrice: price - price * 0.02,
+           maximumPrice: price + price * 0.02});
           console.log(`isVaultCreated: ${isVaultCreated}`);
           break;
         case 3: const isTokenMinted = await acc.mintToken({amount: 2,
-          vault: new Vault({name: "goEth", network: acc.network}),
+          vault: new Vault({name: 'lfty0314', network: acc.network}),
           minimumPrice: price - price * 0.02,
           maximumPrice: price + price * 0.02});
           console.log(`isTokenMinted: ${isTokenMinted}`);
           break;
         case 4: const isVaultDebtReturned = await acc
             .returnVaultDebt({amount: 4,
-              vault: new Vault({name: "goEth", network: acc.network}),
+              vault: new Vault({name: 'lfty0314', network: acc.network}),
               address: await acc.getAddress()});
           console.log(`isVaultDebtReturned: ${isVaultDebtReturned}`);
           break;
         case 5: const isCollateralWithdrawn = await acc.
             withdrawCollateral({amount: 0.0001,
-              vault: new Vault({name: "goEth", network: acc.network}), 
+              vault: new Vault({name: 'lfty0314', network: acc.network}),
               minimumPrice: price - price * 0.02,
               maximumPrice: price + price * 0.02});
           console.log(`isCollateralWithdrawn: ${isCollateralWithdrawn}`);
           break;
         case 6: const isCollateralDeposited = await acc.
             depositCollateral({amount: 0.002,
-              vault: new Vault({name: "goEth", network: acc.network})});
+              vault: new Vault({name: 'lfty0314', network: acc.network})});
           console.log(`isCollateralDeposited: ${isCollateralDeposited}`);
+          break;
+        case 7: const isPricecUpdated =
+          await acc.updatePrice({price: 22,
+            vault: new Vault({name: 'lfty0314', network: acc.network})});
+          console.log({isPricecUpdated});
           break;
       }
     } catch (error) {
--- examples/liquidateVault.js
+++ examples/liquidateVault.js
@@ -5,21 +5,21 @@ dotenv.config();
 (async () => {
   const mnemonic = process.env.MNEMONIC;
   const VAULT_ID = process.env.VAULT_ID ? process.env.VAULT_ID :
-  VAULTS.TestNet.algo.vaultId;
+  VAULTS.TestNet.lfty0314.vaultId;
   const liquidationAddress = process.env.ADDRESS_FOR_LIQUIDATION;
   const account = new VaultsClient({
     mnemonic,
     network: 'TestNet',
   });
-  const vault = new Vault({name: "algo", network: "TestNet"});
+  const vault = new Vault({name: 'lfty0314', network: 'TestNet'});
   try {
     const isVaultLiquidated = await account.liquidateVault({
       address: liquidationAddress,
-      debtAmount: 30,
+      debtAmount: 19.8,
       vault,
       dripInterest: true,
-      minimumPrice: 1,
-      maximumPrice: 1});
+      minimumPrice: 22,
+      maximumPrice: 22});
     console.log(`isVaultLiquidated: ${isVaultLiquidated}`);
   } catch (error) {
     console.error(error);
