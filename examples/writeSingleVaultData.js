4dc2b515f7e38c084240963c6cf742660ee1fa1b
jesulonimi21 <anjola4jeez@gmail.com>
2024-11-15 22:13:26 +0100

    update: nov 15

--- examples/writeSingleVaultData.js
+++ examples/writeSingleVaultData.js
@@ -54,7 +54,7 @@ function writeToJson(filePath, data) {
       // indexer port
       '',
   );
-  const vaultName = 'algo';
+  const vaultName = 'xAlgo';
   Object.entries({[vaultName]: VAULTS['MainNet'][vaultName]})
       .forEach(async ([name, rawData]) =>{
         console.log({name});
