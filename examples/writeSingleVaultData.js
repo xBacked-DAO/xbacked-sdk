31af013dd5f79c19a73091f55b321a88a970840c
jesulonimi21 <anjola4jeez@gmail.com>
2024-11-26 21:07:15 +0000

    update: nov 26

--- examples/writeSingleVaultData.js
+++ examples/writeSingleVaultData.js
@@ -54,7 +54,7 @@ function writeToJson(filePath, data) {
       // indexer port
       '',
   );
-  const vaultName = 'compx';
+  const vaultName = 'ora';
   Object.entries({[vaultName]: VAULTS['MainNet'][vaultName]})
       .forEach(async ([name, rawData]) =>{
         console.log({name});
